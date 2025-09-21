f8d4bd2d44f225346d73993e7d2df4632d874f09
sevru <sevru@DESKTOP-NFMFIIS>
2025-09-21 21:57:30 +1000

    DZ DONE;

--- Academy/MainForm.Designer.cs
+++ Academy/MainForm.Designer.cs
@@ -29,25 +29,45 @@
 		private void InitializeComponent()
 		{
 			this.statusStrip = new System.Windows.Forms.StatusStrip();
+			this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
 			this.tabControl = new System.Windows.Forms.TabControl();
 			this.tabPageStudents = new System.Windows.Forms.TabPage();
 			this.tabPageGroups = new System.Windows.Forms.TabPage();
 			this.tabPageDirections = new System.Windows.Forms.TabPage();
+			this.dataGridViewDirections = new System.Windows.Forms.DataGridView();
 			this.tabPageDisciplines = new System.Windows.Forms.TabPage();
 			this.tabPageTeachers = new System.Windows.Forms.TabPage();
-			this.dataGridViewDirections = new System.Windows.Forms.DataGridView();
+			this.dataGridViewDisciplines = new System.Windows.Forms.DataGridView();
+			this.dataGridViewStudents = new System.Windows.Forms.DataGridView();
+			this.dataGridViewGroups = new System.Windows.Forms.DataGridView();
+			this.dataGridViewTeachers = new System.Windows.Forms.DataGridView();
+			this.statusStrip.SuspendLayout();
 			this.tabControl.SuspendLayout();
+			this.tabPageStudents.SuspendLayout();
+			this.tabPageGroups.SuspendLayout();
 			this.tabPageDirections.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridViewDirections)).BeginInit();
+			this.tabPageDisciplines.SuspendLayout();
+			this.tabPageTeachers.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridViewDisciplines)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridViewGroups)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridViewTeachers)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// statusStrip
 			// 
+			this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
 			this.statusStrip.Location = new System.Drawing.Point(0, 428);
 			this.statusStrip.Name = "statusStrip";
 			this.statusStrip.Size = new System.Drawing.Size(800, 22);
 			this.statusStrip.TabIndex = 0;
-			this.statusStrip.Text = "statusStrip1";
+			// 
+			// toolStripStatusLabel
+			// 
+			this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+			this.toolStripStatusLabel.Size = new System.Drawing.Size(0, 17);
 			// 
 			// tabControl
 			// 
@@ -62,9 +82,11 @@
 			this.tabControl.SelectedIndex = 0;
 			this.tabControl.Size = new System.Drawing.Size(800, 428);
 			this.tabControl.TabIndex = 1;
+			this.tabControl.SelectedIndexChanged += new System.EventHandler(this.tabControl_SelectedIndexChanged_1);
 			// 
 			// tabPageStudents
 			// 
+			this.tabPageStudents.Controls.Add(this.dataGridViewStudents);
 			this.tabPageStudents.Location = new System.Drawing.Point(4, 22);
 			this.tabPageStudents.Name = "tabPageStudents";
 			this.tabPageStudents.Padding = new System.Windows.Forms.Padding(3);
@@ -75,6 +97,7 @@
 			// 
 			// tabPageGroups
 			// 
+			this.tabPageGroups.Controls.Add(this.dataGridViewGroups);
 			this.tabPageGroups.Location = new System.Drawing.Point(4, 22);
 			this.tabPageGroups.Name = "tabPageGroups";
 			this.tabPageGroups.Padding = new System.Windows.Forms.Padding(3);
@@ -94,8 +117,19 @@
 			this.tabPageDirections.Text = "Directions";
 			this.tabPageDirections.UseVisualStyleBackColor = true;
 			// 
+			// dataGridViewDirections
+			// 
+			this.dataGridViewDirections.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+			this.dataGridViewDirections.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridViewDirections.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dataGridViewDirections.Location = new System.Drawing.Point(3, 3);
+			this.dataGridViewDirections.Name = "dataGridViewDirections";
+			this.dataGridViewDirections.Size = new System.Drawing.Size(786, 396);
+			this.dataGridViewDirections.TabIndex = 0;
+			// 
 			// tabPageDisciplines
 			// 
+			this.tabPageDisciplines.Controls.Add(this.dataGridViewDisciplines);
 			this.tabPageDisciplines.Location = new System.Drawing.Point(4, 22);
 			this.tabPageDisciplines.Name = "tabPageDisciplines";
 			this.tabPageDisciplines.Padding = new System.Windows.Forms.Padding(3);
@@ -106,6 +140,7 @@
 			// 
 			// tabPageTeachers
 			// 
+			this.tabPageTeachers.Controls.Add(this.dataGridViewTeachers);
 			this.tabPageTeachers.Location = new System.Drawing.Point(4, 22);
 			this.tabPageTeachers.Name = "tabPageTeachers";
 			this.tabPageTeachers.Size = new System.Drawing.Size(792, 402);
@@ -113,27 +148,66 @@
 			this.tabPageTeachers.Text = "Teachers";
 			this.tabPageTeachers.UseVisualStyleBackColor = true;
 			// 
-			// dataGridViewDirections
+			// dataGridViewDisciplines
 			// 
-			this.dataGridViewDirections.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dataGridViewDirections.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.dataGridViewDirections.Location = new System.Drawing.Point(3, 3);
-			this.dataGridViewDirections.Name = "dataGridViewDirections";
-			this.dataGridViewDirections.Size = new System.Drawing.Size(786, 396);
-			this.dataGridViewDirections.TabIndex = 0;
+			this.dataGridViewDisciplines.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+			this.dataGridViewDisciplines.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridViewDisciplines.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dataGridViewDisciplines.Location = new System.Drawing.Point(3, 3);
+			this.dataGridViewDisciplines.Name = "dataGridViewDisciplines";
+			this.dataGridViewDisciplines.Size = new System.Drawing.Size(786, 396);
+			this.dataGridViewDisciplines.TabIndex = 0;
 			// 
-			// Form1
+			// dataGridViewStudents
+			// 
+			this.dataGridViewStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridViewStudents.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dataGridViewStudents.Location = new System.Drawing.Point(3, 3);
+			this.dataGridViewStudents.Name = "dataGridViewStudents";
+			this.dataGridViewStudents.Size = new System.Drawing.Size(786, 396);
+			this.dataGridViewStudents.TabIndex = 0;
+			// 
+			// dataGridViewGroups
+			// 
+			this.dataGridViewGroups.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridViewGroups.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dataGridViewGroups.Location = new System.Drawing.Point(3, 3);
+			this.dataGridViewGroups.Name = "dataGridViewGroups";
+			this.dataGridViewGroups.Size = new System.Drawing.Size(786, 396);
+			this.dataGridViewGroups.TabIndex = 0;
+			// 
+			// dataGridViewTeachers
+			// 
+			this.dataGridViewTeachers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridViewTeachers.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dataGridViewTeachers.Location = new System.Drawing.Point(0, 0);
+			this.dataGridViewTeachers.Name = "dataGridViewTeachers";
+			this.dataGridViewTeachers.Size = new System.Drawing.Size(792, 402);
+			this.dataGridViewTeachers.TabIndex = 0;
+			// 
+			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
 			this.Controls.Add(this.tabControl);
 			this.Controls.Add(this.statusStrip);
-			this.Name = "Form1";
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Academy PD_411";
+			this.statusStrip.ResumeLayout(false);
+			this.statusStrip.PerformLayout();
 			this.tabControl.ResumeLayout(false);
+			this.tabPageStudents.ResumeLayout(false);
+			this.tabPageGroups.ResumeLayout(false);
 			this.tabPageDirections.ResumeLayout(false);
 			((System.ComponentModel.ISupportInitialize)(this.dataGridViewDirections)).EndInit();
+			this.tabPageDisciplines.ResumeLayout(false);
+			this.tabPageTeachers.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridViewDisciplines)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridViewGroups)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridViewTeachers)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -149,6 +223,11 @@
 		private System.Windows.Forms.TabPage tabPageDisciplines;
 		private System.Windows.Forms.TabPage tabPageTeachers;
 		private System.Windows.Forms.DataGridView dataGridViewDirections;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+		private System.Windows.Forms.DataGridView dataGridViewDisciplines;
+		private System.Windows.Forms.DataGridView dataGridViewStudents;
+		private System.Windows.Forms.DataGridView dataGridViewGroups;
+		private System.Windows.Forms.DataGridView dataGridViewTeachers;
 	}
 }
 
--- Academy/MainForm.cs
+++ Academy/MainForm.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
+using System.Xml.Linq;
 
 namespace Academy
 {
@@ -17,14 +18,15 @@ namespace Academy
 			string connectionString = "Data Source=DESKTOP-NFMFIIS\\SQLEXPRESS;Initial Catalog=PD_321;Integrated Security=True;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 		public MainForm()
 		{
+			
 			InitializeComponent();
 			connection = new SqlConnection(connectionString);
 
 			LoadDirections();
-
-
-
-
+			LoadDisciplines();
+			LoadStudents();
+			LoadGroups();
+			LoadTeachers();
 
 		}
 
@@ -51,6 +53,143 @@ namespace Academy
 			reader.Close();
 			connection.Close();
 			dataGridViewDirections.DataSource = table;
+			UpdateStatusBar();
+		}
+
+		void LoadDisciplines() 
+		{
+			string cmd = " SELECT * FROM Disciplines";
+			SqlCommand command = new SqlCommand(cmd, connection);
+
+			connection.Open();
+			SqlDataReader reader = command.ExecuteReader();
+			DataTable table = new DataTable();
+
+			for (int i = 0; i < reader.FieldCount; i++)
+				table.Columns.Add(reader.GetName(i));
+
+			while (reader.Read())
+			{
+				DataRow row = table.NewRow();
+				for (int i = 0; i < reader.FieldCount; i++)
+					row[i] = reader[i];
+
+				table.Rows.Add(row);
+			}
+			reader.Close();
+			connection.Close();
+			dataGridViewDisciplines.DataSource = table;
+			//toolStripStatusLabel.Text = $"Количество строк: {table.Rows.Count}";
+			UpdateStatusBar();
+		}
+
+		void LoadStudents()
+		{
+			string cmd = " SELECT * FROM Students";
+			SqlCommand command = new SqlCommand(cmd, connection);
+
+			connection.Open();
+			SqlDataReader reader = command.ExecuteReader();
+			DataTable table = new DataTable();
+
+			for (int i = 0; i < reader.FieldCount; i++)
+				table.Columns.Add(reader.GetName(i));
+
+			while (reader.Read())
+			{
+				DataRow row = table.NewRow();
+				for (int i = 0; i < reader.FieldCount; i++)
+					row[i] = reader[i];
+
+				table.Rows.Add(row);
+			}
+			reader.Close();
+			connection.Close();
+			dataGridViewStudents.DataSource = table;
+			//toolStripStatusLabel.Text = $"Количество строк: {table.Rows.Count}";
+			UpdateStatusBar();
+		}
+
+		void LoadGroups()
+		{
+			string cmd = " SELECT * FROM Groups";
+			SqlCommand command = new SqlCommand(cmd, connection);
+
+			connection.Open();
+			SqlDataReader reader = command.ExecuteReader();
+			DataTable table = new DataTable();
+
+			for (int i = 0; i < reader.FieldCount; i++)
+				table.Columns.Add(reader.GetName(i));
+
+			while (reader.Read())
+			{
+				DataRow row = table.NewRow();
+				for (int i = 0; i < reader.FieldCount; i++)
+					row[i] = reader[i];
+
+				table.Rows.Add(row);
+			}
+			reader.Close();
+			connection.Close();
+			dataGridViewGroups.DataSource = table;
+			UpdateStatusBar();
+		}
+
+		void LoadTeachers()
+		{
+			string cmd = " SELECT * FROM Teachers";
+			SqlCommand command = new SqlCommand(cmd, connection);
+
+			connection.Open();
+			SqlDataReader reader = command.ExecuteReader();
+			DataTable table = new DataTable();
+
+			for (int i = 0; i < reader.FieldCount; i++)
+				table.Columns.Add(reader.GetName(i));
+
+			while (reader.Read())
+			{
+				DataRow row = table.NewRow();
+				for (int i = 0; i < reader.FieldCount; i++)
+					row[i] = reader[i];
+
+				table.Rows.Add(row);
+			}
+			reader.Close();
+			connection.Close();
+			dataGridViewTeachers.DataSource = table;
+			//toolStripStatusLabel.Text = $"Количество строк: {table.Rows.Count}";
+			UpdateStatusBar();
+		}
+
+		void UpdateStatusBar()
+		{
+			if (tabControl.SelectedTab == tabPageDirections)
+			{
+				toolStripStatusLabel.Text = $"Количество строк: {dataGridViewDirections.Rows.Count - 1}";
+			}
+			else if (tabControl.SelectedTab == tabPageStudents)
+			{
+				toolStripStatusLabel.Text = $"Количество строк: {dataGridViewStudents.Rows.Count - 1}";
+			}
+			else if (tabControl.SelectedTab == tabPageTeachers)
+			{
+				toolStripStatusLabel.Text = $"Количество строк: {dataGridViewTeachers.Rows.Count - 1}";
+			}
+			else if (tabControl.SelectedTab == tabPageGroups)
+			{
+				toolStripStatusLabel.Text = $"Количество строк: {dataGridViewGroups.Rows.Count - 1}";
+			}
+			else if (tabControl.SelectedTab == tabPageDisciplines)
+			{
+				toolStripStatusLabel.Text = $"Количество строк: {dataGridViewDisciplines.Rows.Count}";
+			}
+		}
+		
+		private void tabControl_SelectedIndexChanged_1(object sender, EventArgs e)
+		{
+			UpdateStatusBar();
 		}
 	}
 }
